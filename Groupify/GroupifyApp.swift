725f9481249eb0ee7711834fc5dd96c726c7e499
David Garcia Romero <DavidIjsud@gmail.com>
2026-03-03 16:18:37 -0400

    Initial Commit

---
+++ Groupify/GroupifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupifyApp.swift
+//  Groupify
+//
+//  Created by David Garcia on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct GroupifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GroupifyTests/GroupifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  GroupifyTests.swift
+//  GroupifyTests
+//
+//  Created by David Garcia on 3/3/26.
+//
+
+import Testing
+@testable import Groupify
+
+struct GroupifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
